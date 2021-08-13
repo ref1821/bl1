7a5d4bbf507e939ad3c22caeeb5d295b3d627a9d
ref1821 <ref1821@gmail.com>
2021-08-13 16:05:19 -0500

    13/8 -1

--- src/components/Pl.js
+++ src/components/Pl.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './play.css'
 import {useState} from 'react'
 import How from './How'
+import New from './new'
+import cnum3 from './new'
 
 
 
@@ -23,72 +25,27 @@ const Pl = () => {
     const rey = 10;
 
     var num = new Array(as, dos, tres, cuatro, cinco, seis, siete, ocho ,nueve, diez, joto, reina, rey);
-    var ale = new Array('yes', 'yes', 'yes', 'no', 'no')
-
-    var dnum = num[Math.floor(Math.random()*num.length)];
-    var dnum2 = num[Math.floor(Math.random()*num.length)];
+    
     var cnum = num[Math.floor(Math.random()*num.length)];
     var cnum2 = num[Math.floor(Math.random()*num.length)];
-    var alet = ale[Math.floor(Math.random()*num.length)];
     
-
-    const [totalUno, setNumbers] = useState(dnum+dnum2)
-    const [cTot, setTot] = useState(cnum+cnum2)
-    const [num1, setNum1] = useState(dnum)
-    const [num2, setNum2] = useState(dnum2)
+    
     const [numc, setNum3] = useState(cnum)
     const [numc2, setNum4] = useState(cnum2)
 
 
-    const Cpu21 = () => {
-        if(cTot<=21 & cTot>=15){
-            if (alet === 'yes'){
-                var cnum3 = num[Math.floor(Math.random()*num.length)];
-                setTot(+cnum3)
-                return(
-                <p>hola</p>
-                )
-                }
-                else if(alet === 'no'){
-                    return(
-                        <p>no</p>
-                    )
-                }
-            }
-            else if(cTot<15){
-                var cnum4 = num[Math.floor(Math.random()*num.length)];
-                setTot(+cnum4)
-                return(
-                    <p>hola</p>
-                )
-            }
-            else {
-                return(
-                    <p>Lost</p>
-                )
-            }
-        }
-    
 
     return (
         <div className="play">
-            
-                <How className='how'></How>
-            
-            <div className="person">
-                <h2 className='letter'>Your numbers</h2>
-                <p>Your numbers are {num1} and {num2}</p>
-                <h4>Total: {totalUno}</h4>
-                <button >New number</button>
-                <h1></h1>
-            </div>
+            <How className='how'></How>
+            <New></New>
             <div className='compu'>
                 <h2 className='letter'>Computer's numbers</h2>
                 <p>Computer's numbers are {numc} and {numc2}</p>
                 
             </div>
             <div>
-                <button onClick={Cpu21}>Finish Game</button>
+                <button>Finish Game</button>
             </div>
         </div>
     )
--- src/components/new.js
+++ src/components/new.js
@@ -1,20 +1,90 @@
 import React, { Component } from 'react'
-
 export class New extends Component {
     
     constructor(props) {
         super(props)
-    
+        const as = 1;
+        const dos = 2;
+        const tres = 3;
+        const cuatro = 4;
+        const cinco = 5;
+        const seis = 6;
+        const siete = 7;
+        const ocho = 8;
+        const nueve = 9;
+        const diez = 10;
+        const joto = 10;
+        const reina = 10;
+        const rey = 10;
+
+        var num = new Array(as, dos, tres, cuatro, cinco, seis, siete, ocho ,nueve, diez, joto, reina, rey);
+        var dnum=num[Math.floor(Math.random()*num.length)];
+        var dnum2=num[Math.floor(Math.random()*num.length)];
+
         this.state = {
-             
+            n:'',
+            num:dnum,
+            num2:dnum2,
+            cnum3: 0,
+            totalUno:dnum+dnum2,
+            qwe: 0,
         }
+        
+        
+        
+        
+    }
+
+    NN(){
+        const as = 1;
+        const dos = 2;
+        const tres = 3;
+        const cuatro = 4;
+        const cinco = 5;
+        const seis = 6;
+        const siete = 7;
+        const ocho = 8;
+        const nueve = 9;
+        const diez = 10;
+        const joto = 10;
+        const reina = 10;
+        const rey = 10;
+
+        var num = new Array(as, dos, tres, cuatro, cinco, seis, siete, ocho ,nueve, diez, joto, reina, rey);
+        var ale = new Array('yes', 'yes', 'yes', 'no', 'no')
+        if (this.state.totalUno<=21){
+            this.setState({
+                cnum3: num[Math.floor(Math.random()*num.length)],
+            },
+            ()=>{
+                this.setState({
+                    n:this.state.n+'your new number is '+this.state.cnum3+'---------------------',
+                    totalUno: this.state.totalUno+this.state.cnum3,
+                })
+            })
+        }
+        else if(this.state.qwe===0&&this.state.totalUno>21){
+            this.setState({
+                n:this.state.n+'you lost',
+                qwe:this.state.qwe+1
+            })
+            
+            console.log(this.state.qwe)
+        }
+        
+        
     }
     
-
     render() {
+        
+        
         return (
-            <div>
-                
+            <div className="person">
+                <h2 className='letter'>Your numbers</h2>
+                <p>Your numbers are {this.state.num} and {this.state.num2}</p>
+                <p>Total: {this.state.totalUno}</p>
+                <button onClick={() => this.NN()}>New number</button>
+                <p>{this.state.n}</p>
             </div>
         )
     }
